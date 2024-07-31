cd17b1d9ded0a6aab5f4e358a2ba401a36711ffe
yuta1993 <nagoyuta1031@gmail.com>
2024-07-31 09:04:44 +0900

    エラーコメントの修正

--- app_2.py
+++ app_2.py
@@ -95,5 +95,5 @@ except Exception as e:
     print(e) # division by zero
     print(f"{e.__class__.__name__}: {e}") # ZeroDivisionError: division by zero
     st.error(
-        "エラーが起きているようです。"
+        "エラーが起きているようです！"
     )
